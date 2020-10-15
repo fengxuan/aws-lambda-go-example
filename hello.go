c4f1919e574134f6e849e39c14fdd9dff929ea77
fengxuan <xuanjianfeng@163.com>
2020-10-15 22:55:37 +0800

    h

--- hello.go
+++ hello.go
@@ -1,13 +1,12 @@
-package main
+package hello
 
 import (
 	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+func handlerHello(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	return &events.APIGatewayProxyResponse{
 		StatusCode:        200,
 		Headers:           map[string]string{"Content-Type": "text/plain"},
@@ -15,9 +14,4 @@ func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResp
 		Body:              "Hello, World!",
 		IsBase64Encoded:   false,
 	}, nil
-}
-
-func main() {
-	// Make the handler available for Remote Procedure Call by AWS Lambda
-	lambda.Start(handler)
 }
